3c0025f59676814c8e3d69e087740641214655cf
delta! <176697709+deltaskul@users.noreply.github.com>
2026-03-25 23:56:51 +0000

    Implement update listener and miner restart logic
    Added functionality to listen for update commands and perform updates.

--- quarry/updater.lua
+++ quarry/updater.lua
@@ -1,1 +1,39 @@
+-- rednet.broadcast("update") triggers the update
 
+local url = ""
+local programName = "quarry"
+
+rednet.open("left")
+
+-- Function 1: Run the miner
+local function runMiner()
+    while true do
+        print("Starting miner...")
+        shell.run(programName)
+        print("Miner stopped. Restarting in 3s...")
+        sleep(3)
+    end
+end
+
+-- Function 2: Listen for update command
+local function listenForUpdate()
+    while true do
+        local id, msg = rednet.receive()
+
+        if msg == "update" then
+            print("Updating program...")
+
+            if shell.run("wget", url, programName .. ".new") then
+                shell.run("rm " .. programName)
+                shell.run("mv " .. programName .. ".new " .. programName)
+                print("Update complete!")
+
+                os.reboot() -- restart to apply cleanly
+            else
+                print("Update failed.")
+            end
+        end
+    end
+end
+
+parallel.waitForAny(runMiner, listenForUpdate)
